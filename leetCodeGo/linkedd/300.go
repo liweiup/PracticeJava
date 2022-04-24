7c203ca5c9dd6f7e140ad63cce34c5a113ea1211
liwei <liwei2@miduchina.com>
2022-04-24 09:30:23 +0800

    master

---
+++ leetCodeGo/linkedd/300.go
@@ -0,0 +1,10 @@
+package main
+
+func lengthOfLis() {
+	arr := []int{0, 1, 0, 3, 2, 3}
+
+	for _, v := range arr {
+		flagNum := v
+		
+	}
+}
--- leetCodeGo/linkedd/main.go
+++ leetCodeGo/linkedd/main.go
@@ -2,13 +2,37 @@ package main
 
 import "fmt"
 
+func reverseList3(nodeList *ListNode) *ListNode {
+	if nodeList.Next == nil {
+		return nodeList
+	}
+	endNode := reverseList3(nodeList.Next)
+	nodeList.Next.Next = nodeList
+	nodeList.Next = nil
+	return endNode
+}
+
+func reverseList4(head *ListNode) *ListNode {
+	temp := ListNode{}
+	for {
+		if head == nil {
+			break
+		}
+		next := head.Next
+		head.Next = temp.Next
+		temp.Next = head
+		head = next
+	}
+	return temp.Next
+}
+
 func main() {
 	// list1 := ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: nil}}}
 	// list2 := ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: nil}}}
 	// nx := mergeTwoLists2(&list1, &list2)
 	list3 := ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4, Next: nil}}}
 	// nx := reverseList(&list3)
-	nx := reverseList2(&list3)
+	nx := reverseList4(&list3)
 	// fmt.Println(nx)
 
 	for {
